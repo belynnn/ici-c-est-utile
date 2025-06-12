e41eb5dfe7599ab3eca4e746c2cabb940acd8fa7
Déborah <deborahclerckx@gmail.com>
2025-06-12 16:21:23 +0200

    test pour rendre l'app pseudo-responsive 8

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -3,7 +3,11 @@ import 'leaflet/dist/leaflet.css';
 
 export default function MapView({ pois }) {
   return (
-    <MapContainer center={[50.8503, 4.3517]} zoom={12}>
+    <MapContainer
+      center={[50.8503, 4.3517]}
+      zoom={12}
+      style={{ width: '100%', height: '70vh' }}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {pois.map((poi, i) => (
         <Marker key={i} position={[poi.lat, poi.lng]}>
