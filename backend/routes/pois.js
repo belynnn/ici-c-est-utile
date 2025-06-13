2c2bb86b510fd673825b6bdf4f6161da31526a5a
Déborah <deborahclerckx@gmail.com>
2025-06-13 15:45:22 +0200

    correction rouge pois - require d'express doit être tout en haut

--- backend/routes/pois.js
+++ backend/routes/pois.js
@@ -1,6 +1,6 @@
+const express = require('express');
 const router = express.Router();
 const verify = require('../middlewares/verify.js');
-const express = require('express');
 const POI = require('../models/POI');
 
 // Lire tous les POIs
