402800e005545186d27099c5f277ddc6950f08c6
Déborah <deborahclerckx@gmail.com>
2025-06-14 14:44:55 +0200

    still trying

--- backend/index.js
+++ backend/index.js
@@ -36,5 +36,5 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/pois', poisRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
